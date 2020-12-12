e09c9e7b347d43b859b56ca6c733c0214de5bac5
JoshEhrlich <josh.ehrlich@queensu.ca>
2020-12-11 23:08:16 -0500

    full push

--- UltrasoundSegmentation/Spine/models.py
+++ UltrasoundSegmentation/Spine/models.py
@@ -8,8 +8,6 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
 from tensorflow.keras.regularizers import l1, l2
 
-
-
 def old_unet(input_size, num_classes, num_channels=1, filter_multiplier=10, regularization_rate=0.):
     input_ = Input((input_size, input_size, num_channels))
     skips = []
@@ -51,7 +49,6 @@ def old_unet(input_size, num_classes, num_channels=1, filter_multiplier=10, regu
 def weighted_categorical_crossentropy(class_weights):
     """
     A weighted version of keras.objectives.categorical_crossentropy
-
     Variables:
         class_weights: numpy array of shape (C,) where C is the number of classes
     """
@@ -75,7 +72,6 @@ def weighted_categorical_crossentropy(class_weights):
 def weighted_categorical_crossentropy_with_maps(class_weights):
     """
     A weighted version of keras.objectives.categorical_crossentropy
-
     Variables:
         class_weights: numpy array of shape (C,) where C is the number of classes
     """
@@ -96,10 +92,7 @@ def weighted_categorical_crossentropy_with_maps(class_weights):
         # clip to prevent NaN's and Inf's
         y_pred = tf.keras.backend.clip(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
         # calc
-        loss = y_true * tf.keras.backend.log(y_pred)
-        loss *= weight_map
-        loss *= class_weights
-        #loss = tf.reduce_sum(loss, axis=-1)
+        loss = y_true * tf.keras.backend.log(y_pred) * weight_map * class_weights
         loss = -tf.keras.backend.sum(loss, -1)
 
         return loss
@@ -219,46 +212,35 @@ def new_unet(
 
     """
     Customisable UNet architecture (Ronneberger et al. 2015 [1]).
-
     Arguments:
     input_shape: 3D Tensor of shape (x, y, num_channels)
-
     num_classes (int): Unique classes in the output mask. Should be set to 1 for binary segmentation
-
     activation (str): A keras.activations.Activation to use. ReLu by default.
-
     use_batch_norm (bool): Whether to use Batch Normalisation across the channel axis between convolutional layers
-
     upsample_mode (one of "deconv" or "simple"): Whether to use transposed convolutions or simple upsampling in the decoder part
-
     dropout (float between 0. and 1.): Amount of dropout after the initial convolutional block. Set to 0. to turn Dropout off
-
     dropout_change_per_layer (float between 0. and 1.): Factor to add to the Dropout after each convolutional block
-
     dropout_type (one of "spatial" or "standard"): Type of Dropout to apply. Spatial is recommended for CNNs [2]
-
     use_dropout_on_upsampling (bool): Whether to use dropout in the decoder part of the network
-
     use_attention (bool): Whether to use an attention dynamic when concatenating with the skip-connection, implemented as proposed by Oktay et al. [3]
-
     filters (int): Convolutional filters in the initial convolutional block. Will be doubled every block
-
     num_layers (int): Number of total layers in the encoder not including the bottleneck layer
-
     output_activation (str): A keras.activations.Activation to use. Sigmoid by default for binary segmentation
-
     Returns:
     model (keras.models.Model): The built U-Net
-
     Raises:
     ValueError: If dropout_type is not one of "spatial" or "standard"
-
-
     [1]: https://arxiv.org/abs/1505.04597
     [2]: https://arxiv.org/pdf/1411.4280.pdf
     [3]: https://arxiv.org/abs/1804.03999
-
     """
+    filters = 16
+    num_layers = 4
+    dropout= 0.3
+    num_classes = 1
+    num_channels = 1
+    dropout_change_per_layer = 0.0
+    
 
     if upsample_mode == "deconv":
         upsample = upsample_conv
@@ -266,12 +248,15 @@ def new_unet(
         upsample = upsample_simple
 
     # Build U-Net model
-    input_shape = (input_size, input_size, num_channels)
+    input_shape = (128, 128, 1) #(input_size, input_size, num_channels)
+    print(input_shape)
     inputs = Input(input_shape)
     x = inputs
 
     down_layers = []
-    for l in range(num_layers):
+    filters = 16
+    print(filters)
+    for l in range(4):
         x = conv2d_block(
             inputs=x,
             filters=filters,
